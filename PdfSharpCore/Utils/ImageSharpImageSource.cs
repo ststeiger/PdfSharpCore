ae1eab222fd2a91465882d2cc4193c3e6c4b02d2
Paul Kratt <Sappharad@users.noreply.github.com>
2019-07-12 09:36:43 -0500

    Fixed PNG detection

--- PdfSharpCore/Utils/ImageSharpImageSource.cs
+++ PdfSharpCore/Utils/ImageSharpImageSource.cs
@@ -1,9 +1,9 @@
-﻿using SixLabors.ImageSharp;
+using SixLabors.ImageSharp;
 using SixLabors.ImageSharp.PixelFormats;
 using MigraDocCore.DocumentObjectModel.MigraDoc.DocumentObjectModel.Shapes;
 using System;
 using System.IO;
-using SixLabors.ImageSharp.Advanced;
+using SixLabors.ImageSharp.Formats;
 using SixLabors.ImageSharp.Formats.Bmp;
 using SixLabors.ImageSharp.Formats.Jpeg;
 using SixLabors.ImageSharp.Formats.Png;
@@ -14,88 +14,41 @@ namespace PdfSharpCore.Utils
     {
         protected override IImageSource FromBinaryImpl(string name, Func<byte[]> imageSource, int? quality = 75)
         {
-            return new ImageSharpImageSourceImpl<TPixel>(name, () =>
-            {
-                return Image.Load<TPixel>(imageSource.Invoke());
-            }, (int)quality);
+            var image = Image.Load<TPixel>(imageSource.Invoke(), out IImageFormat imgFormat);
+            return new ImageSharpImageSourceImpl<TPixel>(name, image, (int)quality, imgFormat is PngFormat);
         }
 
         protected override IImageSource FromFileImpl(string path, int? quality = 75)
         {
-            return new ImageSharpImageSourceImpl<Rgba32>(path, () => { return Image.Load<Rgba32>(path); },
-                (int) quality);
+            var image = Image.Load<TPixel>(path, out IImageFormat imgFormat);
+            return new ImageSharpImageSourceImpl<TPixel>(path, image, (int) quality, imgFormat is PngFormat);
         }
 
         protected override IImageSource FromStreamImpl(string name, Func<Stream> imageStream, int? quality = 75)
         {
-            return new ImageSharpImageSourceImpl<TPixel>(name, () =>
+            using (var stream = imageStream.Invoke())
             {
-                using (var stream = imageStream.Invoke())
-                {
-                    return Image.Load<TPixel>(stream);
-                }
-            }, (int)quality);
+                var image = Image.Load<TPixel>(stream, out IImageFormat imgFormat);
+                return new ImageSharpImageSourceImpl<TPixel>(name, image, (int)quality, imgFormat is PngFormat);
+            }
         }
 
         private class ImageSharpImageSourceImpl<TPixel2> : IImageSource where TPixel2 : struct, IPixel<TPixel2>
         {
-
-            private Image<TPixel2> _image;
-            private Image<TPixel2> Image
-            {
-                get
-                {
-                    if (_image == null)
-                    {
-                        _image = _getImage.Invoke();
-                    }
-                    return _image;
-                }
-            }
-            private Func<Image<TPixel2>> _getImage;
+            private Image<TPixel2> Image { get; }
             private readonly int _quality;
 
             public int Width => Image.Width;
             public int Height => Image.Height;
             public string Name { get; }
-            private bool? _transparent;
+            public bool Transparent { get; internal set; }
 
-            public bool Transparent
-            {
-                get
-                {
-                    if (_transparent == null)
-                    {
-                        _transparent = false;
-                        //Note: This is going to result in the image being loaded into memory
-                        //up front, because we need to know if it is transparent or not.
-                        //Previously the image wouldn't be loaded until it was used.
-                        //If this is a problem, we might want to use GetMetaData and change
-                        //how ImageSources are constructed.
-                        if (Image.PixelType.BitsPerPixel == 32)
-                        {
-                            //ImageSharp will create PngMetaData for any image type when calling
-                            //GetFormatMetaData even if it wasn't PNG.
-                            //To avoid generating extra metadata for non-PNG, check for 32-bit color.
-                            //This will not completely prevent extra metadata from being created for
-                            //images that are not PNG, but at least it avoids extra logic sometimes.
-                            PngMetaData pngMeta = _image.MetaData.GetFormatMetaData(PngFormat.Instance);
-                            if (pngMeta != null && (pngMeta.ColorType == PngColorType.RgbWithAlpha ||
-                                                    pngMeta.ColorType == PngColorType.GrayscaleWithAlpha))
-                            {
-                                _transparent = true;
-                            }
-                        }
-                    }
-                    return _transparent ?? false;
-                }
-            }
-
-            public ImageSharpImageSourceImpl(string name, Func<Image<TPixel2>> getImage, int quality)
+            public ImageSharpImageSourceImpl(string name, Image<TPixel2> image, int quality, bool isTransparent)
             {
                 Name = name;
-                _getImage = getImage;
+                Image = image;
                 _quality = quality;
+                Transparent = isTransparent;
             }
 
             public void SaveAsJpeg(MemoryStream ms)
