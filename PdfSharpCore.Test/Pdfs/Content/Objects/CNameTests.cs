a99c64274cc0ccc3492e1c4a01e60e7d4e7679fa
Jason Finch <jason.finch@gmail.com>
2022-06-04 16:25:32 +1000

    fix (Cname): Setting name now validates value rather than previous set private name.
    Credits original author @jeanbern https://github.com/empira/PDFsharp/pull/86

---
+++ PdfSharpCore.Test/Pdfs/Content/Objects/CNameTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using FluentAssertions;
+using PdfSharpCore.Pdf.Content.Objects;
+using Xunit;
+
+namespace PdfSharpCore.Test.Pdfs.Content.Objects
+{
+    public class CNameTests
+    {
+        [Theory]
+        [InlineData("/Foo")]
+        public void SetNameTests(string name)
+        {
+            var cName = new CName
+            {
+                Name = name
+            };
+
+            cName.Name.Should().Be(name);
+        }
+
+        [Fact]
+        public void SetNameNullThrowsException()
+        {
+            Action act = () => new CName
+            {
+                Name = null
+            };
+            act.Should().Throw<ArgumentNullException>();
+        }
+
+        [Theory]
+        [InlineData("Foo")]
+        [InlineData("")]
+        public void SetNameWithoutPrefixThrowsException(string name)
+        {
+            Action act = () => new CName
+            {
+                Name = name
+            };
+            act.Should().Throw<ArgumentException>();
+        }
+    }
+}
